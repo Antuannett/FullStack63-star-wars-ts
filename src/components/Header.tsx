a804fa1a10709dedd8def197e5a1542327f97b33
adeli <adelinapribega@gmail.com>
2026-05-01 19:28:58 +0300

    Refractor and replace Header routing to context use

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,17 @@
 import Navigation from "./Navigation.tsx";
-import {useParams} from "react-router";
-import {characters, defaultHero} from "../utils/constants.ts";
+import {characters} from "../utils/constants.ts";
+import {useContext} from "react";
+import {SWContext} from "../utils/context.ts";
 
 const Header = () => {
-    const {heroId = defaultHero} = useParams()
-    if (heroId in characters) {
+    const {hero} = useContext(SWContext)
     return (
         <header className="rounded-t-3xl bg-gray">
             <Navigation/>
-            <h1 className="text-center text-4xl py-6">{characters[heroId as keyof typeof characters].name}</h1>
+            <h1 className="text-center text-4xl py-6">{characters[hero].name}</h1>
         </header>
-    ) } else {
-      return (
-          <header className="rounded-t-3xl bg-gray">
-              <Navigation/>
-              <h1 className="text-center text-4xl py-6">O-ops invalid hero</h1>
-          </header>
-      )
-    }
+    )
+
 }
 
 export default Header;
